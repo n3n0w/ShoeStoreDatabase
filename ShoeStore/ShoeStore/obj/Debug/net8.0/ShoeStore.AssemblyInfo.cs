748c919c3f30dd8975215d6e213b5d9b7f70a62f
n3n0w <nenovkrasi@gmail.com>
2025-06-06 22:12:28 +0300

    update

--- ShoeStore/ShoeStore/obj/Debug/net8.0/ShoeStore.AssemblyInfo.cs
+++ ShoeStore/ShoeStore/obj/Debug/net8.0/ShoeStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoeStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9af3274bc7b8638eda72c560bffdc8bf60ee2d04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b141d816ab7a4ccbfcb1de56315193c832a4764d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoeStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoeStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
